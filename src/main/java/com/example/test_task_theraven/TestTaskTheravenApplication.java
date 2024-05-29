c037a537b40652495fff08131a3b986402113984
Bohdan Kaziamyr <notpadumink01@gmail.com>
2024-05-29 14:19:37 +0300

    second commit

---
+++ src/main/java/com/example/test_task_theraven/TestTaskTheravenApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_task_theraven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskTheravenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestTaskTheravenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test_task_theraven/TestTaskTheravenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_task_theraven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskTheravenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
